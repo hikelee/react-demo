9cc4f6217d324525732f5053d18d321f43721545
hikelee <hikelee@gmail.com>
2018-12-17 18:53:24 +0800

    init

---
+++ server/snippets/views.py
@@ -0,0 +1,58 @@
+from collections import OrderedDict
+
+from django.contrib.auth.models import User
+from rest_framework import permissions, renderers, viewsets
+from rest_framework.decorators import detail_route
+from rest_framework.response import Response
+
+from snippets.models import Snippet, Member
+from snippets.permissions import IsOwnerOrReadOnly
+from snippets.serializers import SnippetSerializer, UserSerializer, MemberSerializer
+
+
+class SnippetViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
+
+    Additionally we also provide an extra `highlight` action.
+    """
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
+
+    @detail_route(renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    This viewset automatically provides `list` and `detail` actions.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class MemberViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list` and `detail` actions.
+    """
+    queryset = Member.objects.all()
+    serializer_class = MemberSerializer
+
+    def perform_create(self, serializer):
+        # serializer.save(owner=self.request.user)
+        serializer.save()
+
+    def get_paginated_response(self, data):
+        count = self.paginator.page.paginator.count
+        return Response(OrderedDict([
+            ('count', count),
+            ('results', data)
+        ]))
+
